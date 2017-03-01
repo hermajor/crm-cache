a5bf57c2a90cc1455a9eff0e818b514cdad46cea
hermajor <hotdog451@gmail.com>
2017-03-01 09:29:24 +0300

    add param IBLOCK_TYPE

--- crm.essence/.parameters.php
+++ crm.essence/.parameters.php
@@ -1,2 +1,23 @@
-<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+if(!CModule::IncludeModule("iblock"))
+	return;
+
+$arIBlockType = CIBlockParameters::GetIBlockTypes();
+
+$arComponentParameters = array(
+	"GROUPS" => array(
+		),
+	"PARAMETERS" => array(
+		"IBLOCK_TYPE" => array(
+			"PARENT" => "BASE",
+			"NAME" => "IBLOCK_TYPE",
+			"TYPE" => "LIST",
+			"ADDITIONAL_VALUES" => "Y",
+			"VALUES" => $arIBlockType,
+			"REFRESH" => "Y",
+		),
+	),
+);
 ?>
--- crm.essence/component.php
+++ crm.essence/component.php
@@ -2,38 +2,29 @@
 
 if(!isset($arParams["CACHE_TIME"]))
 	$arParams["CACHE_TIME"] = 36000000;
-//echo "<pre>";print_r($this);
 
 if($this->startResultCache(false, array(($arParams["CACHE_GROUPS"]==="N"? false: $USER->GetGroups())))){
-
-	$IBLOCK_ID = 14;	
 		
-	if(CModule::IncludeModule("iblock") && ($arIBlock = GetIBlock($IBLOCK_ID))) {
+	if(CModule::IncludeModule("iblock")) {
 
-	$arSelect = Array("IBLOCK_ID", "ID", "NAME", "PROPERTY_PROP_CLIENT", "PROPERTY_COMPANY", "PROPERTY_GROUP");
-	$arFilter = Array("IBLOCK_ID"=>$IBLOCK_ID, "ACTIVE"=>"Y");
+	$arSelect = Array("IBLOCK_ID", "ID", "NAME");
+	$arFilter = Array("IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"], "ACTIVE" => "Y");
 	
 	$arResult = array();
 	$res = CIBlockElement::GetList(Array(), $arFilter, false, Array(), $arSelect);
-	/*while($ob = $res->GetNextElement()){
-	   $arFields = $ob->GetFields();
-	   echo "<pre>"; print_r($arFields); echo "</pre>";
-	}*/
-	
-	while($arFields = $res->GetNext())
-	{
-			$arResult[$arFields['ID']] = array(
-					'NAME' => $arFields['NAME'], 
-					'PROP_CLIENT' => $arFields['PROPERTY_PROP_CLIENT_VALUE'], 
-					'COMPANY' => $arFields['PROPERTY_COMPANY_VALUE'], 
-					'GROUP' => $arFields['PROPERTY_GROUP_VALUE']
-					);
+		while($ob = $res->GetNextElement()){
+			$arFields = $ob->GetFields();
+			$props = $ob->GetProperties();
+			$arTemp = array(
+				'NAME' => $arFields['NAME'],
+				'GROUP' => $props['GROUP']['VALUE'],
+				'PROP_CLIENT' => $props['PROP_CLIENT']['VALUE'],
+				'COMPANY' => $props['COMPANY']['VALUE']
+			);
+			$arResult[] = $arTemp;
+		}
 	}
-		
-	} else {
-		ShowError("Информационный блок не найден.");
-	}
-
-        $this->IncludeComponentTemplate();
 }
+
+return $arResult;
 ?>
